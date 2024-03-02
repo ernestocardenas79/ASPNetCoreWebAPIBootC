9ac5f3f3ae23262cca9fb26820dab3505383ca1b
UNOSQUARE\ernesto.cardenas <ernesto.cardenas@unosquare.com>
2024-03-01 18:45:26 -0700

    initial commit

---
+++ LeerData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeerData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola alumnos al curso ASP .NET Core - React Hooks");
+        }
+    }
+}
